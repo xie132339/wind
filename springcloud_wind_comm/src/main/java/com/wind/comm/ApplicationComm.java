7abc55f72e9702cf236949115028adadcbec1afb
xiedengfeng <xiedengfeng@lvmama.com>
2018-10-16 19:27:38 +0800

    springcloud

---
+++ springcloud_wind_comm/src/main/java/com/wind/comm/ApplicationComm.java
@@ -0,0 +1,11 @@
+package com.wind.comm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationComm {
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationComm.class, args);
+	}
+}
