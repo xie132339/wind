48a55b243895ede34c2161d9aa391dcd190d54f9
xiedengfeng <xiedengfeng@lvmama.com>
2018-10-17 16:13:14 +0800

    ApplicationComm 测试

--- springcloud_wind_comm/src/main/java/com/wind/comm/ApplicationComm.java
+++ springcloud_wind_comm/src/main/java/com/wind/comm/ApplicationComm.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApplicationComm {
 	public static void main(String[] args) {
+		System.out.println("test");
 		SpringApplication.run(ApplicationComm.class, args);
 	}
 }
