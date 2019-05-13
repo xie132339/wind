70bcf611d7bfd5fca4c49ae1ef802b2a60c6e7e0
xiedengfeng <xiedengfeng@lvmama.com>
2019-05-13 19:50:09 +0800

    修改

--- springcloud_wind_order/src/main/java/com/wind/order/OrderApplication.java
+++ springcloud_wind_order/src/main/java/com/wind/order/OrderApplication.java
@@ -12,6 +12,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 public class OrderApplication {
     public static void main(String[] args) {
-	SpringApplication.run(OrderApplication.class, args);
+        SpringApplication.run(OrderApplication.class, args);
     }
 }
--- springcloud_wind_order/src/main/java/com/wind/order/controller/OrderController.java
+++ springcloud_wind_order/src/main/java/com/wind/order/controller/OrderController.java
@@ -5,8 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping("/order")
+@Api(tags = "订单")
 public class OrderController {
     @Value("${server.port}")
     private String port;
@@ -20,6 +24,7 @@ public class OrderController {
      * @return
      */
     @GetMapping("/getOrderPort")
+    @ApiOperation(value = "订单测试")
     public String getOrderPort() {
         return "order-service port：" + port + "--" + info;
     }
