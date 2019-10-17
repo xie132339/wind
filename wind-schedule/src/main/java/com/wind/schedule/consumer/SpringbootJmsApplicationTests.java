7068a43ad54ebf05760181b2429c19bc118968a0
Administrator <Administrator@192.168.0.101>
2019-10-17 23:40:09 +0800

    修改pom

--- wind-schedule/src/main/java/com/wind/schedule/consumer/SpringbootJmsApplicationTests.java
+++ wind-schedule/src/main/java/com/wind/schedule/consumer/SpringbootJmsApplicationTests.java
@@ -3,14 +3,12 @@ package com.wind.schedule.consumer;
 import javax.jms.Destination;
 
 import org.apache.activemq.command.ActiveMQQueue;
-import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class SpringbootJmsApplicationTests {
     @Autowired
     private Producer producer;
 
-    @Test
     public void contextLoads() throws InterruptedException {
 	Destination destination = new ActiveMQQueue("mytest.queue");
 	for (int i = 0; i < 100; i++) {
